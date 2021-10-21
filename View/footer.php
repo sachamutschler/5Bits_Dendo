c928cb7439b5643d04b8e129e3e46ca9047488a6
Sacha Mutschler <[sacha.mutschler@mediaschool.me]>
2021-10-21 19:08:43 +0200

    ajout du footer et de bootstrap

---
+++ View/footer.php
@@ -0,0 +1,29 @@
+<div class="footer-dark">
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-md-3 item">
+                    <h3>Services</h3>
+                    <ul>
+                        <li><a href="#">Vente de vélo</a></li>
+                        <li><a href="#">Maintenance</a></li>
+                        <li><a href="#">Conseils</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-md-3 item">
+                    <h3>About</h3>
+                    <ul>
+                        <li><a href="#">L'entreprise</a></li>
+                        <li><a href="#">L'équipe</a></li>
+                        <li><a href="#">Nos offres</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 item text">
+                    <h3>Dendo Jitensha</h3>
+                    <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                </div>
+            </div>
+            <p class="copyright">Dendo Jitensha © 2021</p>
+        </div>
+    </footer>
+</div>
