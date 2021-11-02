5c41502a5d1d2ebf2734449819092c308036ece2
Sacha <sacha.mutschler@mediaschool.me>
2021-11-02 11:08:11 +0100

    correction conflits

---
+++ View/produits.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php include ('head.php'); ?>
+</head>
+<body>
+</html>
+
+<div id="header">
+    <?php include ('navbar.php'); ?>
+</div>
+
+<div class="contenu">
+    <h1 class="titre_page">Nos produits</h1>
+</div>
+
+
+</body>
