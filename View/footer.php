186660323c4d5c2d79e46122239ad917218dbbb0
Sacha <sacha.mutschler@mediaschool.me>
2021-11-02 10:38:58 +0100

    ajout du footer sans bootstrap

--- View/footer.php
+++ View/footer.php
@@ -1,29 +1,32 @@
-<div class="footer-dark">
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6 col-md-3 item">
-                    <h3>Services</h3>
-                    <ul>
-                        <li><a href="#">Vente de vélo</a></li>
-                        <li><a href="#">Maintenance</a></li>
-                        <li><a href="#">Conseils</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-6 col-md-3 item">
-                    <h3>About</h3>
-                    <ul>
-                        <li><a href="#">L'entreprise</a></li>
-                        <li><a href="#">L'équipe</a></li>
-                        <li><a href="#">Nos offres</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-6 item text">
-                    <h3>Dendo Jitensha</h3>
-                    <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
-                </div>
-            </div>
-            <p class="copyright">Dendo Jitensha © 2021</p>
-        </div>
-    </footer>
+<div class="cont_footer">
+
+<footer class="footer">
+    <div class="services">
+        <h3>Services</h3>
+        <ul class="cont_lien_footer">
+            <li><a href="#" class="lien_footer">Vente de vélo</a></li>
+            <li><a href="#" class="lien_footer">Maintenance</a></li>
+            <li><a href="#" class="lien_footer">Conseils</a></li>
+        </ul>
+    </div>
+    <div class="About">
+        <h3>A propos</h3>
+        <ul class="cont_lien_footer">
+            <li><a href="#" class="lien_footer">L'entreprise</a></li>
+            <li><a href="#" class="lien_footer">L'équipe</a></li>
+            <li><a href="#" class="lien_footer">Nos offres</a></li>
+        </ul>
+    </div>
+    <div class="footer_desc">
+        <h3>Dendo Jitensha</h3>
+        <p class="text_desc">Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+
+    </div>
+    
+</footer>
+<div>
+    <p class="copyright">Dendo Jitensha © 2021</p>
+</div>
+
+
 </div>
