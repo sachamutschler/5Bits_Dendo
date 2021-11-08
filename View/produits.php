bb665b69cf183916ecbb4751a1791f3fe2fe0fc6
stacoprod <91543842+stacoprod@users.noreply.github.com>
2021-11-08 10:26:17 +0100

    ajout de la branche aurelien

--- View/produits.php
+++ View/produits.php
@@ -8,11 +8,131 @@
 
 <div id="header">
     <?php include ('navbar.php'); ?>
+    <link href="public/css/produits.css" rel="stylesheet">
 </div>
 
 <div class="contenu">
-    <h1 class="titre_page">Nos produits</h1>
+<div class="colonne_gauche">
+</div>
+<div class="colonne_droite">
+    <div id="articles_produits">
+        <div class="article_produits">
+            <div class="article_container">
+                <img class="image" src="public/images/produits/image_produits_1.png" alt="produit_1">
+                <h3>Produit 1</h3>
+                <h4>1099 € <strike>1299€</strike></h4>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+            <a class="bouton_produit" href="#">Produit 1</a>
+        </div>
+        <div class="article_produits">
+            <div class="article_container">
+                <img class="image" src="public/images/produits/image_produits_2.png" alt="produit_2">
+                <h3>Produit 2</h3>
+                <h4>1099 € <strike>1299€</strike></h4>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+            <a class="bouton_produit" href="#">Produit 2</a>
+        </div>
+        <div class="article_produits">
+            <div class="article_container">
+                <img class="image" src="public/images/produits/image_produits_3.png" alt="produit_3">
+                <h3>Produit 3</h3>
+                <h4>1099 € <strike>1299€</strike></h4>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+            <a class="bouton_produit" href="#">Produit 3</a>
+        </div>
+        <div class="article_produits">
+            <div class="article_container">
+                <img class="image" src="public/images/produits/image_produits_3.png" alt="produit_4">
+                <h3>Produit 4</h3>
+                <h4>1099 € <strike>1299€</strike></h4>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+            <a class="bouton_produit" href="#">Produit 4</a>
+        </div>
+        
+    </div>
+    <div id="articles_produits">
+        <div class="article_produits">
+            <div class="article_container">
+                <img class="image" src="public/images/produits/image_produits_1.png" alt="produit_1">
+                <h3>Produit 1</h3>
+                <h4>1099 € <strike>1299€</strike></h4>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+            <a class="bouton_produit" href="#">Produit 1</a>
+        </div>
+        <div class="article_produits">
+            <div class="article_container">
+                <img class="image" src="public/images/produits/image_produits_2.png" alt="produit_2">
+                <h3>Produit 2</h3>
+                <h4>1099 € <strike>1299€</strike></h4>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+            <a class="bouton_produit" href="#">Produit 2</a>
+        </div>
+        <div class="article_produits">
+            <div class="article_container">
+                <img class="image" src="public/images/produits/image_produits_3.png" alt="produit_3">
+                <h3>Produit 3</h3>
+                <h4>1099 € <strike>1299€</strike></h4>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+            <a class="bouton_produit" href="#">Produit 3</a>
+        </div>
+        <div class="article_produits">
+            <div class="article_container">
+                <img class="image" src="public/images/produits/image_produits_3.png" alt="produit_4">
+                <h3>Produit 4</h3>
+                <h4>1099 € <strike>1299€</strike></h4>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+            <a class="bouton_produit" href="#">Produit 4</a>
+        </div>
+        
+    </div>
+    <div id="articles_produits">
+        <div class="article_produits">
+            <div class="article_container">
+                <img class="image" src="public/images/produits/image_produits_1.png" alt="produit_1">
+                <h3>Produit 1</h3>
+                <h4>1099 € <strike>1299€</strike></h4>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+            <a class="bouton_produit" href="#">Produit 1</a>
+        </div>
+        <div class="article_produits">
+            <div class="article_container">
+                <img class="image" src="public/images/produits/image_produits_2.png" alt="produit_2">
+                <h3>Produit 2</h3>
+                <h4>1099 € <strike>1299€</strike></h4>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+            <a class="bouton_produit" href="#">Produit 2</a>
+        </div>
+        <div class="article_produits">
+            <div class="article_container">
+                <img class="image" src="public/images/produits/image_produits_3.png" alt="produit_3">
+                <h3>Produit 3</h3>
+                <h4>1099 € <strike>1299€</strike></h4>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+            <a class="bouton_produit" href="#">Produit 3</a>
+        </div>
+        <div class="article_produits">
+            <div class="article_container">
+                <img class="image" src="public/images/produits/image_produits_3.png" alt="produit_4">
+                <h3>Produit 4</h3>
+                <h4>1099 € <strike>1299€</strike></h4>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+            <a class="bouton_produit" href="#">Produit 4</a>
+        </div>
+        
+    </div>
 </div>
 
-
+    
 </body>
