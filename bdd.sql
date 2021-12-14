e8b0535661918c5aa49fe2d4f62cdac8481fad38
Fab <91542864+Fab1189@users.noreply.github.com>
2021-12-14 17:49:28 +0100

    Fichier création bdd 14/12
    Fichier pour créer la base, il fonctionne :-)

--- bdd.sql
+++ bdd.sql
@@ -1,40 +1,5 @@
-CREATE TABLE Historique
-(
-    ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    ID_Commande INT NOT NULL,
-    ID_CompteClient INT NOT NULL
-)
-
-CREATE TABLE Panier
-(
-    ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    quantite INT DEFAULT NULL,
-    ID_Produit INT NOT NULL,
-)
-
-CREATE TABLE Categorie
-(
-    ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    nom VARCHAR(50) NOT NULL
-)
-
-CREATE TABLE SousCategorie
-(
-    ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    nom VARCHAR (50) NOT NULL,
-    ID_Categorie INT NOT NULL
-)
-
-CREATE TABLE Categorie_Produit
-(
-    ID_Categorie INT NOT NULL,
-    ID_Produit INT NOT NULL
-)
-
-CREATE TABLE Produit
-(
-    ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+CREATE TABLE Produit(
+    idProduit INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     reference VARCHAR(255) DEFAULT NULL,
     designation VARCHAR(255) DEFAULT NULL,
     poids INT(4) DEFAULT NULL,
@@ -47,57 +12,86 @@ CREATE TABLE Produit
     stock INT(4) DEFAULT NULL,
     prix INT(6) DEFAULT NULL,
     image VARCHAR(255) DEFAULT NULL,
-    electrique TINYINT(1) DEFAULT 0
-)
+    electrique TINYINT(1) NOT NULL DEFAULT 0
+);
 
-CREATE TABLE Commande
+CREATE TABLE Categorie
 (
-    idCommande INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    transporteur VARCHAR(255) DEFAULT NULL,
-    code_colis VARCHAR(255) DEFAULT NULL,
-    poids INT(4) DEFAULT NULL,
-    taille INT(3) DEFAULT NULL,
-    etat VARCHAR(25) DEFAULT NULL,
-    ID_CompteClient INT NOT NULL
-)
+    idCategorie INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL
+);
 
-CREATE TABLE LigneCommande
+CREATE TABLE SousCategorie
 (
-    ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    quantite INT(4) DEFAULT NULL,
-    montant_unite INT(4) DEFAULT NULL,
-    montant_total INT(4) DEFAULT NULL,
-    reference_produit VARCHAR(255) DEFAULT NULL,
-    designation_produit VARCHAR(255) DEFAULT NULL,
-    ID_Commande INT NOT NULL
-)
+    idSousCategorie INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL,
+    idCategorie INT NOT NULL
+);
 
-CREATE TABLE CompteCLient
+CREATE TABLE Categorie_Produit
 (
-    ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    identifiant VARCHAR(55) DEFAULT NULL,
-    mot_de_passe VARCHAR(255) DEFAULT NULL,
-    nom VARCHAR(255) DEFAULT NULL,
-    prenom VARCHAR(255) DEFAULT NULL,
-    mail VARCHAR(255) DEFAULT NULL,
-    telephone_port INT(25) DEFAULT NULL,
-    telephone_fixe INT(25) DEFAULT NULL,
-    adresse1 VARCHAR(255) DEFAULT NULL,
-    adresse2 VARCHAR(255) DEFAULT NULL,
-    ville VARCHAR(255) DEFAULT NULL,
-    code_postal INT(10) DEFAULT NULL,
-    pays VARCHAR(255) DEFAULT NULL,
-    code_validation VARCHAR(255) DEFAULT NULL,
-    etat VARCHAR(100) DEFAULT NULL
-)
+    idSousCategorie INT NOT NULL,
+    idCategorie INT NOT NULL
+);
 
-CREATE TABLE Promo
-(
-    ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+CREATE TABLE Promo(
+    idPromo INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     code VARCHAR(255) DEFAULT NULL,
     reduction DECIMAL(2) DEFAULT NULL,
     duree_validite TIME DEFAULT NULL,
     date_debut DATETIME DEFAULT NULL,
     date_fin DATETIME DEFAULT NULL,
-    ID_Produit INT NOT NULL
-)
+    idProduit INT NOT NULL
+);
+
+CREATE TABLE Commande(
+     idCommande INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+     transporteur VARCHAR(255) DEFAULT NULL,
+     code_colis VARCHAR(255) DEFAULT NULL,
+     poids INT(4) DEFAULT NULL,
+     taille INT(3) DEFAULT NULL,
+     etat VARCHAR(25) DEFAULT NULL,
+     idCompte_client INT NOT NULL
+);
+
+CREATE TABLE Ligne_commande(
+    idLigne_commande INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    quantite INT(4) DEFAULT NULL,
+    montant_unite INT(4) DEFAULT NULL,
+    montant_total INT(6) DEFAULT NULL,
+    reference_produit VARCHAR (255) DEFAULT NULL,
+    designation_produit VARCHAR (255) DEFAULT NULL,
+    idCommande INT NOT NULL,
+    idProduit INT NOT NULL
+);
+
+CREATE TABLE Historique(
+    idHistorique INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    idLigne_commande INT NOT NULL,
+    idCompte_client INT NOT NULL
+);
+
+CREATE TABLE Panier(
+    idPanier INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    quantite INT (4) DEFAULT NULL,
+    idProduit INT NOT NULL
+);
+
+CREATE TABLE Compte_client(
+    idCompte_client INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    identifiant VARCHAR (255) NOT NULL,
+    mot_de_passe VARCHAR (255) NOT NULL,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR (255) NOT NULL,
+    mail VARCHAR (255) NOT NULL,
+    telephone_port INT(25) NOT NULL,
+    telephone_fixe INT(25) DEFAULT NULL,
+    adresse_1 VARCHAR(255) NOT NULL,
+    adresse_2 VARCHAR (255) DEFAULT NULL,
+    ville VARCHAR(255) NOT NULL,
+    code_postal VARCHAR(60) NOT NULL,
+    pays VARCHAR(255) NOT NULL,
+    code_validation VARCHAR(255) DEFAULT NULL,
+    etat VARCHAR(255) DEFAULT NULL
+);
