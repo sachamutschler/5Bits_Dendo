c9d4a00333526e30217af0294343d9e8a057c938
Sachamutschler <sacha.mutschler@mediaschool.me>
2022-02-22 17:25:44 +0100

    Modifications multiples au niveau de la page contact et du css et head

--- View/navbar.php
+++ View/navbar.php
@@ -2,7 +2,7 @@
 <div id="navbar">
     <a class="navbar_link" href="index.php">Accueil</a>
     <a class="navbar_link" href="produits.php">Produits</a>
-    <a class="navbar_link" href="#">Contact</a>
+    <a class="navbar_link" href="contact.php">Contact</a>
     <a class="navbar_link" href="#">Qui sommes nous ?</a>
     <a class="navbar_link" href="#">Mon compte</a>
 </div>
---
+++ contact.php
@@ -0,0 +1,2 @@
+<?php 
+require('./View/contact.php');
