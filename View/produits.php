cabb588189694a9d66fdfdad4dfd78208e680f7d
stacoprod <91543842+stacoprod@users.noreply.github.com>
2022-03-01 16:50:46 +0100

    ajout modif page produits

--- View/produits.php
+++ View/produits.php
@@ -1,148 +1,90 @@
+<?php
+function connexionBase($nomBase)
+{
+    $servername="localhost";
+    $username="root";
+    $password="";
+    try {
+        $db = new PDO("mysql:host=$servername;dbname=$nomBase", $username, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e) {
+        echo "Error of connection: " .$e->getMessage();
+        die();
+    }
+    return $db;
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <?php include ('head.php'); ?>
-</head>
-<body>
+    <head>
+        <?php include ('head.php'); ?>
+    </head>
+    <body>
+        <div id="header">
+            <link href="public/css/produits.css" rel="stylesheet">
+            <?php include ('navbar.php'); ?>
+        </div>
+        <div class="contenu">
+            <h1 class="titre_page">Nos produits</h1>
+                <input class=recherche type=text name="recherche" placeholder="Recherche..."><label for="recherche"></label>
+                    <div class="colonnes_container">
+                        <div class="colonne_gauche">
+                            <!-- Boutons de tri + requêtage -->
+                            <table>
+                                <?php $db = connexionBase('dendo');
+                                    $requete = $db->query("select idCategorie, nom FROM categorie");
+                                    $requete = $requete->fetchAll();
+                                    echo '<tr>
+                                            <form method="get">
+                                               <button type="submit" value="%" name="categorie">Tout</button>
+                                            </form>
+                                           </tr>';
+                                    foreach ($requete as $item){
+                                        echo'<tr>
+                                                <form method="get">
+                                                    <button type="submit" value="'.$item['idCategorie'].'" name="categorie">'.$item['nom'].'</button>
+                                                </form>
+                                             </tr>';
+                                    }
+                                    ?>
+                            </table>
+
+                        </div>
+                        <div class="colonne_droite">
+                            <div id="articles_produits">
+                                <?php if (isset($_GET['categorie']) !=0){
+                                        $categorie  = $_GET['categorie'];
+                                    } else {
+                                        $categorie = "%";}
+                                    $requete2 = $db->prepare("select * FROM produit p
+                                                                INNER JOIN taxonomie_produit tp on p.idProduit = tp.idProduit
+                                                                INNER JOIN taxonomie t on tp.idTaxonomie = t.idTaxonomie
+                                                                INNER JOIN categorie c on p.idCategorie = c.idCategorie
+                                                                WHERE p.idCategorie LIKE :categorie");
+                                    $requete2->bindValue('categorie', $categorie);
+                                    $requete2->execute();
+                                    $requete2 = $requete2->fetchAll();
+                                    foreach ($requete2 as $item2){
+                                        $prixSpecial=$item2['prix']-50;
+                                        ?>
+                                        <div class="article_produits">
+                                            <div class="article_container">
+                                                <img class="image" src="<?= $item2['image']; ?>" alt="image_produit_1">
+                                                <h3><?= $item2['designation']; ?></h3>
+                                                <h4><?= $prixSpecial ?>€ <strike><?= $item2['prix'] ?>€</strike></h4>
+                                                <p>Ce vélo <?php if ($item2['electrique']){echo ' électrique ';} echo"possède le dernier type de suspension ".$item2['type_suspension']." ainsi qu'un système de freinage ".$item2['type_frein'].". Le cadre en ".$item2['matiere_cadre']." est particulièrement indiqué pour vos sorties".$item2['nom'];?></p>
+
+                                            </div>
+                                            <a class="bouton_produit" href="#">Yatama</a>
+                                        </div>
+                                    <?php }?>
+
+                            </div>
+                        </div>
+                    </div>
+        </div>
+    </body>
 </html>
-
-<div id="header">
-    <?php include ('navbar.php'); ?>
-    <link href="public/css/produits.css" rel="stylesheet">
-</div>
-
-<div class="contenu">
-<h1 class="titre_page">Nos produits</h1>
-<input class=recherche type=text name="recherche" placeholder="Recherche..."><label for="recherche"></label>
-<div class="colonnes_container">
-<div class="colonne_gauche">
-    <table>
-        <tr><td>Electrique</td></tr>
-        <tr><td>Ville</td></tr>
-        <tr><td>VTT</td></tr>
-        <tr><td>Homme</td></tr>
-        <tr><td>Femme</td></tr>
-        <tr><td>Enfant</td></tr>
-        <tr><td>Personnalisable</td></tr>
-        <tr><td>Meilleures ventes</td></tr>
-        <tr><td>Trier par prix</td></tr>
-    </table>
-</div>
-<div class="colonne_droite">
-    <div id="articles_produits">
-        <div class="article_produits">
-            <div class="article_container">
-                <img class="image" src="public/images/produits/image_produits_1.png" alt="produit_1">
-                <h3>Yatama</h3>
-                <h4>1099 € <strike>1199€</strike></h4>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            </div>
-            <a class="bouton_produit" href="#">Yatama</a>
-        </div>
-        <div class="article_produits">
-            <div class="article_container">
-                <img class="image" src="public/images/produits/image_produits_2.png" alt="produit_2">
-                <h3>Naganamo</h3>
-                <h4>1199 € <strike>1299€</strike></h4>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            </div>
-            <a class="bouton_produit" href="#">Naganamo</a>
-        </div>
-        <div class="article_produits">
-            <div class="article_container">
-                <img class="image" src="public/images/produits/image_produits_3.png" alt="produit_3">
-                <h3>Syanumi</h3>
-                <h4>1049 € <strike>1199€</strike></h4>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            </div>
-            <a class="bouton_produit" href="#">Syanumi</a>
-        </div>
-        <div class="article_produits">
-            <div class="article_container">
-                <img class="image" src="public/images/produits/image_produits_3.png" alt="produit_4">
-                <h3>Mamabobo</h3>
-                <h4>1199 € <strike>1299€</strike></h4>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            </div>
-            <a class="bouton_produit" href="#">Mamabobo</a>
-        </div>
-        <div class="article_produits">
-            <div class="article_container">
-                <img class="image" src="public/images/produits/image_produits_1.png" alt="produit_1">
-                <h3>Hiroshima</h3>
-                <h4>1399 € <strike>1499€</strike></h4>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            </div>
-            <a class="bouton_produit" href="#">Hiroshima</a>
-        </div>
-        <div class="article_produits">
-            <div class="article_container">
-                <img class="image" src="public/images/produits/image_produits_2.png" alt="produit_2">
-                <h3>Ninjagumi</h3>
-                <h4>1499 € <strike>1599€</strike></h4>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            </div>
-            <a class="bouton_produit" href="#">Ninjagumi</a>
-        </div>
-        <div class="article_produits">
-            <div class="article_container">
-                <img class="image" src="public/images/produits/image_produits_3.png" alt="produit_3">
-                <h3>Mangavelo</h3>
-                <h4>1099 € <strike>1299€</strike></h4>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            </div>
-            <a class="bouton_produit" href="#">Mangavelo</a>
-        </div>
-        <div class="article_produits">
-            <div class="article_container">
-                <img class="image" src="public/images/produits/image_produits_3.png" alt="produit_4">
-                <h3>Sushiresto</h3>
-                <h4>1099 € <strike>1299€</strike></h4>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            </div>
-            <a class="bouton_produit" href="#">Sushiresto</a>
-        </div>
-        <div class="article_produits">
-            <div class="article_container">
-                <img class="image" src="public/images/produits/image_produits_1.png" alt="produit_1">
-                <h3>Velonuma</h3>
-                <h4>1099 € <strike>1299€</strike></h4>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            </div>
-            <a class="bouton_produit" href="#">Velonuma</a>
-        </div>
-        <div class="article_produits">
-            <div class="article_container">
-                <img class="image" src="public/images/produits/image_produits_2.png" alt="produit_2">
-                <h3>Osaka</h3>
-                <h4>1099 € <strike>1299€</strike></h4>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            </div>
-            <a class="bouton_produit" href="#">Osaka</a>
-        </div>
-        <div class="article_produits">
-            <div class="article_container">
-                <img class="image" src="public/images/produits/image_produits_3.png" alt="produit_3">
-                <h3>Monfuji</h3>
-                <h4>1099 € <strike>1299€</strike></h4>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            </div>
-            <a class="bouton_produit" href="#">Monfuji</a>
-        </div>
-        <div class="article_produits">
-            <div class="article_container">
-                <img class="image" src="public/images/produits/image_produits_3.png" alt="produit_4">
-                <h3>Tokyo</h3>
-                <h4>1099 € <strike>1299€</strike></h4>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            </div>
-            <a class="bouton_produit" href="#">Tokyo</a>
-        </div>
-        
-    </div>
-</div>
-</div>
-</div>
-
-    
-</body>
