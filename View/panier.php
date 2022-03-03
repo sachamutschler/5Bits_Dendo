625a4a5b115320a98b38e82898c0efad164be52b
Sachamutschler <sacha.mutschler@mediaschool.me>
2022-03-03 23:14:50 +0100

    Modif Panier

--- View/panier.php
+++ View/panier.php
@@ -5,17 +5,19 @@
     <?php include ('navbar.php'); ?>
     <link href="public/css/panier.css" rel="stylesheet">
 </div>
-<div class="panier">
-    <img class="imagePanier" src="public/images/accueil/image_accueil_3.png" alt="produit_accueil_3">
-    <div class="text_panier">
-    <form action="post" class="quantity">
-        <label for="title_panier" class="t_panier">Nom du produit</label>
-        <label class="t_panier">Quantité : </label>
-        <label class="t_panier">Prix : </label>
-        <input type="button" value="Supprimer" name="supprimer" class="delete_panier">
-        <a href="https://buy.stripe.com/test_8wMdRba3LdAN5KUdQR"><input type="button" value="Paiement" name="livraison_panier" class="panier_livraison"></a>
+<div class="cont_panier">
+    <div class="panier">
+        <img class="imagePanier" src="public/images/accueil/image_accueil_3.png" alt="produit_accueil_3">
+        <div class="text_panier">
+        <form action="post" class="quantity">
+            <label for="title_panier" class="t_panier">Nom du produit</label>
+            <label class="t_panier">Quantité : </label>
+            <label class="t_panier">Prix : </label>
+            <input type="button" value="Supprimer" name="supprimer" class="delete_panier">
+            <a href="https://buy.stripe.com/test_8wMdRba3LdAN5KUdQR"><input type="button" value="Paiement" name="livraison_panier" class="panier_livraison"></a>
 
-    </form> 
+        </form> 
+        </div>
     </div>
 </div>
 <?php include ('footer.php'); ?>
