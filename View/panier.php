a817f14c4e4ee459faf5d6cd70e967365206dc50
Sachamutschler <sacha.mutschler@mediaschool.me>
2022-03-04 02:06:54 +0100

    Modif page panier avec requete prix

--- View/panier.php
+++ View/panier.php
@@ -1,3 +1,6 @@
+
+
+
 <head>
     <?php include ('head.php'); ?>
 </head>
@@ -6,15 +9,23 @@
     <link href="public/css/panier.css" rel="stylesheet">
 </div>
 <div class="cont_panier">
-    <?php
+    <div class="cont_panier2">
+        <?php
 
-        for ($i=0; $i < 3; $i++) { 
-            require('produit-panier.php');
-        }
+            for ($i=0; $i < 2; $i++) { 
+                require('produit-panier.php');
+            }
 
-    ?>
-    <div class="cont_buy">
-        <a href="https://buy.stripe.com/test_8wMdRba3LdAN5KUdQR"><input type="button" value="Paiement" name="livraison_panier" class="panier_livraison"></a>
+        ?>
+    </div>
+    
+    <div class="total_buy">
+        <div class="cont_total"> 
+            <p class="txt_total">Le montant total est de : </br> <?php echo("$total"); ?> €</p>
+        </div>
+        <div class="cont_buy">
+            <a href="https://buy.stripe.com/test_8wMdRba3LdAN5KUdQR"><input type="button" value="Paiement" name="livraison_panier" class="panier_livraison"></a>
+        </div>
     </div>
 </div>
 <?php include ('footer.php'); ?>
--- View/produit-panier.php
+++ View/produit-panier.php
@@ -1,10 +1,36 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+
+    try
+    {
+        $db = new PDO("mysql:host=$servername;dbname=dendo",$username,'');
+        $db ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    catch(PDOException $e)
+    {
+        echo "Erreur de la connexion : " .$e->getMessage();
+        die();
+    }
+    
+    $total = 0;
+    $requete = 'SELECT prix FROM `produit` WHERE id = 1';
+    $prix = $db->query($requete);
+    $prix->execute();
+    $prix->setFetchMode(PDO::FETCH_CLASS, 'prix');
+    $res = $prix->fetch();
+    
+?>
+
 <div class="panier">
     <img class="imagePanier" src="public/images/accueil/image_accueil_3.png" alt="produit_accueil_3">
     <div class="text_panier">
         <form action="post" class="quantity">
             <label for="title_panier" class="t_panier">Nom du produit</label>
             <label class="t_panier">Quantité : </label>
-            <label class="t_panier">Prix : </label>
+            <label class="t_panier">Prix : <?php echo $res['prix']; ?></label>
             <input type="button" value="Supprimer" name="supprimer" class="delete_panier">
 
         </form> 
