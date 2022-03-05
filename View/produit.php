dd5fe929ba26e90a7bb0794cadec060f8ec506d6
Nihad <nihad.zatric@gmail.com>
2022-03-05 17:10:03 +0100

    page produit nihad

--- View/produit.php
+++ View/produit.php
@@ -38,7 +38,14 @@ if(isset($_GET['id_produit'])) {
                 }
                 ?> €</h2>
                 <p class="caracteristiques_element">Poids : <?php echo($produit[0]['poids']) ?> kg</p>
-
+                <p class="caracteristiques_element">Taille de cadre : <?php echo($taille_cadre[0]['taille_cadre']) ?></p>
+                <p class="caracteristiques_element">Taille des roues : <?php echo($taille_roues[0]['taille_roues']) ?> kg</p>
+                <p class="caracteristiques_element">Couleur : <?php echo($couleur[0]['couleur']) ?></p>
+                <br>
+                <p class="caracteristiques_element">Ce vélo <?php echo($categorie[0]['nom_categorie']) ?> <?php echo($produit[0]['nom_produit']) ?> de couleur <?php echo($couleur[0]['couleur']) ?> est au prix de <?php if($produit[0]['reduction_produit'] != 0) { echo($prix_apres .'€ au lieu de '.$prix_avant);  }else{ echo($produit[0]['prix']); } ?> €</p>
+                <br>
+                <p class="caracteristiques_element">Conceptualisé par nos soins, produit et assemblé par des partenaires de confiance, ce vélo est le fruit de dizaines d'années d'expérience dans le domaine du cyclisme.</p>
+                <button type="button" id="bouton_ajout_au_panier" class="bouton">Ajouter au panier</button>
             </div>
         </div>
     <?php
