20693c3b445fb02170e54b5f3ce7a177a348e47a
Thomas DUFOUR <thomas.dufour57400@gmail.com>
2022-03-07 12:56:44 +0100

    Reglage pb session

--- View/navbar.php
+++ View/navbar.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST['deconnexion'])){
-    $_SESSION['identifiant'] = "";
+    session_destroy();
 }
 ?>
 <img id="icone_menu" onclick="afficher_navbar()" src="public/images/icone_menu.png" alt="menu">
@@ -10,17 +10,17 @@ if (isset($_POST['deconnexion'])){
     <a class="navbar_link" href="contact.php">Contact</a>
     <a class="navbar_link" href="#">Qui sommes nous ?</a>
     <?php
-    if ($_SESSION['identifiant']){
+    if (isset($_SESSION['identifiant'])){
     ?>
     <a class="navbar_link" href="moncompte.php">Mon compte</a>
     <?php
     }
-    if (!$_SESSION['identifiant']){
+    if (!isset($_SESSION['identifiant'])){
         ?>
     <a class="navbar_link" href="connexion.php">Se connecter</a>
     <?php
     }
-    if ($_SESSION['identifiant']){
+    if (isset($_SESSION['identifiant'])){
         ?>
         <form action="index.php" method="post">
             <input type="submit" name="deconnexion" value="Deconnexion">
