5e3d80cff60faa7975916193a60006e1ef6fed2f
fheilig <fabian.heilig@moniris.com>
2022-03-08 14:19:04 +0100

    modif head

--- View/head.php
+++ View/head.php
@@ -5,7 +5,6 @@
 <link rel="stylesheet" href="public/css/accueil.css">
 <link rel="stylesheet" href="public/css/panier.css">
 <link rel="stylesheet" href="public/css/produits.css">
-<link rel="stylesheet" href="public/css/paiement.css">
 <!--<link rel="preconnect" href="https://fonts.googleapis.com">-->
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Smooch+Sans:wght@100&display=swap" rel="stylesheet">
