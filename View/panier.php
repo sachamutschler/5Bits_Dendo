ea53143e4f7f29758bc91ee5fc3a7d15893c360e
Sachamutschler <sacha.mutschler@mediaschool.me>
2022-03-08 21:29:53 +0100

    responsive panier

--- View/panier.php
+++ View/panier.php
@@ -75,14 +75,16 @@ if(isset($_POST['deleteItem'])){
         ?>
     </div>
     
-    <div class="total_buy">
+    
+</div>
+
+<div class="total_buy">
         <div class="cont_total"> 
             <p class="txt_total">Le montant total est de : </br> <?php echo("$total"); ?> €</p>
         </div>
         <div class="cont_buy">
-            <a href="https://buy.stripe.com/test_8wMdRba3LdAN5KUdQR"><input type="button" value="Paiement" name="livraison_panier" class="panier_livraison"></a>
+            <a href="https://buy.stripe.com/test_8wMdRba3LdAN5KUdQR"><input type="button" value="Paiement" name="livraison_panier" class="panier_livraison bouton"></a>
         </div>
     </div>
-</div>
 <?php include ('footer.php'); ?>
 
