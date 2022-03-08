0b5f53829fc3efa48ebe396901b4c0484ab071f7
unknown <nihad.zatric@gmail.com>
2022-03-08 15:13:06 +0100

    page qui sommes nous

--- View/dendo_jitensha.php
+++ View/dendo_jitensha.php
@@ -5,7 +5,7 @@ session_start();
 <html lang="fr">
 <head>
     <?php include ('head.php'); ?>
-    <link href="public/css/accueil.css" rel="stylesheet">
+    <link href="public/css/dendo_jitensha.css" rel="stylesheet">
 
 </head>
 <body>
@@ -14,8 +14,8 @@ session_start();
     <?php include ('navbar.php'); ?>
 </div>
 
-<div class="contenu">
-    <h1 class="titre_page">Dendo Jitensha</h1>
+<div class="contenu" id="contenu_dendo">
+    <h1 class="titre_page">Jitensha</h1>
 
     <div id="presentation">
         <div id="texte_presentation">
@@ -28,17 +28,21 @@ session_start();
                 <br><br>Voila l'esprit et l'énergie JITENSHA !</p>
         </div>
         <img src="public/images/accueil/accueil_1.jpg" alt="accueil_1">
+    </div>
 
-        <img src="public/images/dendo_jitensha/dendo_j_1.jpg" alt="dendo1">
+
+    <h1 class="titre_page">Dendo</h1>
+    <div id="presentation">
         <div id="texte_presentation">
-            <p>JITENSHA signifie vélo en japonais. Le vélo est profondément ancré dans la culture et le style de vie à Tokyo. On y croise toutes sortes de vélos, et des gens d’horizons très différents qui tous, font du vélo. Le vélo est le moyen idéal pour vivre Tokyo.<br>
+            <p>Le design, la qualité et le confort sont au cœur de JITENSHA. Sans compromis! Minimaliste, pur, sobre et french chic font le style JITENSHA. Nos vélos sont inspirés du Japon, designés en France et assemblés-main et sur commande dans notre atelier. <br>
 
-                <br>L’art de vivre japonais a inspiré à JITENSHA la recherche d’essentiel, de pureté, et d’élégance. Une approche minimaliste, qui peut produire des looks pointus, voire extrêmes, comme les jouent les adolescentes de Shibuya ou les bikers tokyoïtes.<br>
+                <br>Une recherche de l’épure qui fait le style JITENSHA: des partis-pris sobres, un branding réduit à l’essentiel, pour des vélos aussi élégants que minimalistes. C’est l’art de vivre japonais qui a inspiré la recherche d’essentiel, d’élégance, de fluidité, de simplicité, de pureté, de poésie.<br>
 
-                <br>Ce clin d’oeil au Japon a guidé tous les choix de JITENSHA, des couleurs de cadres au design des embouts de guidon. L’envie de partager sa vie entre Tokyo, Paris et toutes les cities, la croyance que le vélo est et sera de plus en plus une belle solution de mobilité urbaine, l’engagement pour un environnement durable, à travers des entreprises plus petites et responsables.
+                <br>
 
-                <br><br>Voila l'esprit et l'énergie JITENSHA !</p>
+                <br><br>Nos mécaniciens assemblent entièrement votre JITENSHA dans notre atelier d'Annecy lorsque nous recevons votre commande. Chaque filetage est graissé, chaque boulon serré. Une fois assemblé, chaque JITENSHA est "human-tested" avant d'être emballé et expédié.</p>
         </div>
+        <img src="public/images/dendo_jitensha/dendo_j_1.jpg" alt="dendo1">
     </div>
 </div>
 
