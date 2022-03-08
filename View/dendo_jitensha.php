1ec744301d5295bd62d1d1435827a0cb0e5b11b9
unknown <nihad.zatric@gmail.com>
2022-03-08 15:03:44 +0100

    page qui sommes nous

--- View/dendo_jitensha.php
+++ View/dendo_jitensha.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 ?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -16,8 +15,34 @@ session_start();
 </div>
 
 <div class="contenu">
+    <h1 class="titre_page">Dendo Jitensha</h1>
+
+    <div id="presentation">
+        <div id="texte_presentation">
+            <p>JITENSHA signifie vélo en japonais. Le vélo est profondément ancré dans la culture et le style de vie à Tokyo. On y croise toutes sortes de vélos, et des gens d’horizons très différents qui tous, font du vélo. Le vélo est le moyen idéal pour vivre Tokyo.<br>
+
+                <br>L’art de vivre japonais a inspiré à JITENSHA la recherche d’essentiel, de pureté, et d’élégance. Une approche minimaliste, qui peut produire des looks pointus, voire extrêmes, comme les jouent les adolescentes de Shibuya ou les bikers tokyoïtes.<br>
+
+                <br>Ce clin d’oeil au Japon a guidé tous les choix de JITENSHA, des couleurs de cadres au design des embouts de guidon. L’envie de partager sa vie entre Tokyo, Paris et toutes les cities, la croyance que le vélo est et sera de plus en plus une belle solution de mobilité urbaine, l’engagement pour un environnement durable, à travers des entreprises plus petites et responsables.
+
+                <br><br>Voila l'esprit et l'énergie JITENSHA !</p>
+        </div>
+        <img src="public/images/accueil/accueil_1.jpg" alt="accueil_1">
+
+        <img src="public/images/dendo_jitensha/dendo_j_1.jpg" alt="dendo1">
+        <div id="texte_presentation">
+            <p>JITENSHA signifie vélo en japonais. Le vélo est profondément ancré dans la culture et le style de vie à Tokyo. On y croise toutes sortes de vélos, et des gens d’horizons très différents qui tous, font du vélo. Le vélo est le moyen idéal pour vivre Tokyo.<br>
+
+                <br>L’art de vivre japonais a inspiré à JITENSHA la recherche d’essentiel, de pureté, et d’élégance. Une approche minimaliste, qui peut produire des looks pointus, voire extrêmes, comme les jouent les adolescentes de Shibuya ou les bikers tokyoïtes.<br>
+
+                <br>Ce clin d’oeil au Japon a guidé tous les choix de JITENSHA, des couleurs de cadres au design des embouts de guidon. L’envie de partager sa vie entre Tokyo, Paris et toutes les cities, la croyance que le vélo est et sera de plus en plus une belle solution de mobilité urbaine, l’engagement pour un environnement durable, à travers des entreprises plus petites et responsables.
+
+                <br><br>Voila l'esprit et l'énergie JITENSHA !</p>
+        </div>
+    </div>
 </div>
 
-</body>
+<?php include ('footer.php'); ?>
 
+</body>
 </html>
